2c9d260fc4eb9f53609c4c5f02c2af1b7ba62ba2
TurnNull <husain.7605@gmail.com>
2024-12-27 15:56:59 +0800

    a: Penambahan fungsi buat data mahasiswa

--- struct.cpp
+++ struct.cpp
@@ -1,63 +1,81 @@
 #include <iostream>
+#include <string>
 #include <conio.h>
+
 using namespace std;
 
-
-void dMenu(){
-system("cls");
-cout<<"Aplikasi Tampilan Menu"<<"\n";       
-cout<<"1. Menu Pertama"<<"\n";            
-cout<<"2. Menu Kedua"<<"\n";            
-cout<<"3. Menu Ketiga"<<"\n";           
-cout<<"4. Menu Keempat"<<"\n";            
-cout<<"5. Exit"<<"\n";           
-cout<<"Masukan angka :";        
-
+void menu() {
+    system("cls");
+    cout<<"Aplikasi Struct Data"<<"\n";       
+    cout<<"1. Menu Buat Data"<<"\n";            
+    cout<<"2. Menu Lihat Data"<<"\n";            
+    cout<<"3. Menu Ubah"<<"\n";           
+    cout<<"4. Menu Keempat"<<"\n";            
+    cout<<"5. Exit"<<"\n";           
+    cout<<"Masukan angka :";        
 }
 
-void mPertama(string pesan){
-system("cls");
-cout<<"hallo saya menu "<<pesan;
-getch();
-}
+struct mahasiswa {
+    string nim;
+    string nama;
+    string alamat;
+    float ipk;
+};
 
+mahasiswa sikc[30];
+int pos=-1;
+int userNp;
+void buatData() {
+    system("cls");
+    cout<<"Masukan panjang data : ";
+    cin>>userNp;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    for (pos++; pos < userNp; pos++) { 
+    cout<<"\n---- ssss ----\n";
+        cout << "Masukan NIM: ";
+        getline(cin, sikc[pos].nim);
+
+        cout << "Masukan Nama: ";
+        getline(cin, sikc[pos].nama);
+
+        cout << "Masukan Alamat: ";
+        getline(cin, sikc[pos].alamat);
+
+        cout << "Masukan IPK: ";
+        cin >> sikc[pos].ipk;
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+}
 
 int main() {
 char pl;
-do
-{
-    dMenu();
-    pl=getch();
-  switch (pl)
-  {
-   case '1':
-    /* code */
-    mPertama("pertama");
-    break;
-   case '2':
-    mPertama("ke- dua");
-    /* code */ 
-    break;  
-   case '3':
-    mPertama("ke- tiga");
-    /* code */
-    break;  
-   case '4':
-    mPertama("ke- empat");
-    /* code */
-    break;  
-  case '5':
-    /* code */
-    break;
-  
-  default:
-    system("cls");
-    cout<<"Pilihan Tidak Tersedia";
-    getch();
-    break;
-  }
-
-
-} while (pl!='5');
-  return 0;
+do {
+  menu();
+  pl=getch();
+  switch (pl) {
+    case '1':
+      buatData();
+      /* code */
+      break;
+    case '2':
+      /* code */ 
+      break;  
+    case '3':
+      /* code */
+      break;  
+    case '4':
+      /* code */
+      break;  
+    case '5':
+      /* code */
+      break;
+    default:
+      system("cls");
+      cout<<"Pilihan Tidak Tersedia";
+      getch();
+      break;
+    }
+  } while (pl!='5');
+    return 0;
 }
